15717946be974ab6e66e4ff3bb7050bbc9aecaf8
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2020-12-19 23:23:39 +0600

    Добавьте файлы проекта.

---
+++ HomeNumberSum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeNumberSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            int с;
+            с = a + b;
+            Console.WriteLine(с);
+        }
+    }
+}
